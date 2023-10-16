efc64c4bc65cbc539e6969aabf7789d445dc2558
FrancoCamen <127841356+FrancoCamen@users.noreply.github.com>
2023-10-16 17:05:39 -0400

    update files the tp5 ejercice 6

--- EntregaTP5/arbol/prueba/ejercicio6.py
+++ EntregaTP5/arbol/prueba/ejercicio6.py
@@ -1,7 +1,7 @@
 from arbolFranco import BinaryTree, get_value_from_file
 from random import randint
 
-file_jedi = open("Trabajos/arbol/prueba/jedis.txt")
+file_jedi = open("EntregaTP5/arbol/prueba/jedis.txt")
 read_lines = file_jedi.readlines()
 file_jedi.close()
 
@@ -38,30 +38,30 @@ def bylevel_ranking_specie():
 #Show information about Yoda and Luke 
 def info_Yoda_and_Luke():
     root = name_tree.search("yoda")
-    info = get_value_from_file("Trabajos/arbol/prueba/jedis.txt", root.other_values)
+    info = get_value_from_file("EntregaTP5/arbol/prueba/jedis.txt", root.other_values)
     print("       Yoda information")
     print(info)
     root = name_tree.search("luke skywalker")
-    info = get_value_from_file("Trabajos/arbol/prueba/jedis.txt", root.other_values)
+    info = get_value_from_file("EntregaTP5/arbol/prueba/jedis.txt", root.other_values)
     print("       LUke information")
     print(info)
 
 #Show all Jedi with ranking "Jedi master"
 def jedi_Master():
-    ranking_tree.jedi_Master("Trabajos/arbol/prueba/jedis.txt")
+    ranking_tree.jedi_Master("EntregaTP5/arbol/prueba/jedis.txt")
 
 #Jedi who used green lightsabers
 def jedi_lightsabers():
     print("          Jedi who used green lightsabers")
-    name_tree.jedi_lightsabersG("Trabajos/arbol/prueba/jedis.txt")
+    name_tree.jedi_lightsabersG("EntregaTP5/arbol/prueba/jedis.txt")
 
 #Jedi whose masters are in the archive
 def jedi_master():
-    name_tree.jedi_with_Master("Trabajos/arbol/prueba/jedis.txt")
+    name_tree.jedi_with_Master("EntregaTP5/arbol/prueba/jedis.txt")
 
 #Jedi with togruta or cerean species
 def _jedi_specie():
-    name_tree.jedi_especie("Trabajos/arbol/prueba/jedis.txt")
+    name_tree.jedi_especie("EntregaTP5/arbol/prueba/jedis.txt")
 
 #jedi beginning with A
 def jedi_with_A():
